7c90ffbebc8bb8d8a0fb04e9d1c485b5c64dff96
Rajin Khan <rajin.khan2001@gmail.com>
2023-08-26 18:14:34 +0600

    Add math_functions

---
+++ math_functions.py
@@ -0,0 +1,11 @@
+import math #the same way header files worked in c, or classes did in java, you have modules in python
+#modules are libraries you can import with pre-defined functions, and the name of each module acts as a class whose methods you can access.
+
+x = 2.9
+
+result_1 = round(x)
+result_2 = abs(x) #absolute value of x, so basically the positive of the number
+result_3 = math.ceil(x)
+result_4 = math.sqrt(x)
+
+print(result_3)
