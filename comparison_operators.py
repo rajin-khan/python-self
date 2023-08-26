ac20c53c3d023e01321ee399502043cf33b59dd5
Rajin Khan <rajin.khan2001@gmail.com>
2023-08-26 19:01:08 +0600

    Update comparison_operators

---
+++ comparison_operators.py
@@ -0,0 +1,9 @@
+temperature = 35
+
+if temperature > 30: #in python comparison operators work the same way as every other language, so you're good
+    
+    print("It's a hot day.")
+    
+else:
+    
+    print("It's not a hot day.")
