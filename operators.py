00244a2a458b9d180261ba34e7e4efa916ea8ba4
Rajin Khan <rajin.khan2001@gmail.com>
2023-08-26 17:50:54 +0600

    Add operators

---
+++ operators.py
@@ -0,0 +1,8 @@
+#all arithmetic operators work in python the same way as other languages, except for division
+num_1 = 10
+num_2 = 3
+
+float_result = num_1 / num_2 #here, by default, the result of the division operator returns a float, 3.333...
+int_result = num_1 // num_2 #here, the integer result is returned, so 3
+
+print(float_result)
