fb0b8921653ebe391b0cd7066c329aeca30754f2
Rajin Khan <rajin.khan2001@gmail.com>
2023-08-28 23:24:20 +0600

    Add 2Dlists.py

---
+++ 2Dlists.py
@@ -0,0 +1,10 @@
+#this 2D list could be any arbitrary name, we just decided to name it matrix. you could hardcode any 2D list this way
+matrix = [ 
+    
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+    
+]
+
+print(matrix[0][1])
