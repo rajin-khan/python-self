2845b2c5d2e4f218f116315c4a3e39e8d87eb6b2
Rajin Khan <rajin.khan2001@gmail.com>
2023-09-19 02:17:12 +0600

    Add test

---
+++ test.py
@@ -0,0 +1,9 @@
+print("Hello world!")
+
+num = int(input("Enter the number of times you would like to print your own name: "))
+
+name = input("Now, enter your name: ")
+
+for i in range(num):
+    
+    print(f"Damn, your name is: {name}")
