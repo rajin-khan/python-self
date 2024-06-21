a6957c9043f0058c37b536054f76dfffc0964a46
Rajin Khan <rajin.khan2001@gmail.com>
2024-06-21 19:42:16 +0600

    Finish learning String Methods

---
+++ StringMethods/app.py
@@ -0,0 +1,8 @@
+course = "Python for Beginners"
+print(len(course)) #general function
+print(course.upper()) #methods
+print(course.lower())
+print(course.find("t"))
+print(course.find("Beginners"))
+print(course.replace("Beginners", "Absolute Beginners"))
+print("Python" in course)
