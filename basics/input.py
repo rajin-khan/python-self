4d9a9acfe8d4b5e4c997566079940ab009a1406e
Rajin Khan <rajin.khan2001@gmail.com>
2024-12-23 02:04:31 +0600

    Start basics

---
+++ basics/input.py
@@ -0,0 +1,3 @@
+name = input('this is the prompt: ')
+age = input('age: ')
+print('Hello', name, 'you are', age, "years old.")
---
+++ basics/variables.py
@@ -0,0 +1,6 @@
+hello = 'rajin'
+world = 'world'
+world = hello
+hello = 'ok'
+hello_world = 'hehe'
+print(hello, world, hello_world)
