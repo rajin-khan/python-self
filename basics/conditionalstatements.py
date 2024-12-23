952279d2f2f91057e836b72141ce26f70303adc4
Rajin Khan <rajin.khan2001@gmail.com>
2024-12-23 18:59:14 +0600

    Add consitional statements

---
+++ basics/conditionalstatements.py
@@ -0,0 +1,12 @@
+name = input('Name: ')
+
+if name == 'rajin':
+   print("You are great my g") 
+elif name == 'raju':
+    print("bro you're still kinda amazing?")
+elif name == 'raj':
+    print("bro you're still kinda super amazing damn")
+else:
+    print("bruh you kinda suck")
+
+print('Always do this')
